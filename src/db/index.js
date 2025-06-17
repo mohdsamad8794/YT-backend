d56c918852e67941f12715638c04ca255c5de782
Mohd Samad <mohdsamad8794@gmail.com>
2025-06-17 08:57:52 +0530

    utils code

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,3 @@
-
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { DB_NAME } from '../constents.js';
--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 import connectDB from './db/index.js';
 import { DB_NAME } from './constents.js';
+import { app } from './aap.js';
+
 
 dotenv.config({
     path: './env'
 })
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
+})
+.catch((error)=>{
+    console.log(`Database connection is failed!! ${error}`);
+})
